4213ff137d413e72fb61e10e896af73baad0a805
Akhil Vengassery <akhillav@gmail.com>
2024-06-15 23:42:17 +0530

    db.sql changed: pending, UserShop tables deleted

--- server/db.sql
+++ server/db.sql
@@ -9,7 +9,6 @@ CREATE TABLE IF NOT EXISTS users (
 
 CREATE TABLE IF NOT EXISTS shops (
 	shop_id INTEGER PRIMARY KEY AUTOINCREMENT,
-	user_id INTEGER,
 	shop_name TEXT UNIQUE,
 	shop_location TEXT,
 
@@ -20,9 +19,16 @@ CREATE TABLE IF NOT EXISTS shops (
 	a3_color_cost INTEGER,
 
 	lat NUMBER DEFAULT NULL,
-	lng NUMBER DEFAULT NULL,
+	lng NUMBER DEFAULT NULL
+);
 
-	FOREIGN KEY(user_id) REFERENCES users(user_id)
+CREATE TABLE IF NOT EXISTS shop_staff (
+    user_id INTEGER,
+    shop_id INTEGER,
+	personal_photo TEXT NOT NULL,
+	status TEXT, -- pending | approved
+    FOREIGN KEY (user_id) REFERENCES users(user_id),
+    FOREIGN KEY (shop_id) REFERENCES shops(shop_id)
 );
 
 CREATE TABLE IF NOT EXISTS files (
@@ -33,7 +39,7 @@ CREATE TABLE IF NOT EXISTS files (
     size TEXT NOT NULL, -- A3, A4 ...
     orientation TEXT NOT NULL,
     totalPages INTEGER,
-	pageRange TEXT NOT NULL, 
+	pageRange TEXT NOT NULL,
     copies INTEGER NOT NULL,
     filename TEXT NOT NULL,
 	done BOOLEAN DEFAULT 0,
@@ -57,20 +63,3 @@ CREATE TABLE IF NOT EXISTS routes (
     FOREIGN KEY(shop_id) REFERENCES shops(shop_id)
 );
 
-CREATE TABLE IF NOT EXISTS pending (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    user_id INTEGER,
-    shop_id INTEGER,
-    personal_photo TEXT NOT NULL,
-    proof_of_work TEXT NOT NULL,
-    FOREIGN KEY(user_id) REFERENCES users(user_id),
-    FOREIGN KEY(shop_id) REFERENCES shops(shop_id)
-);
-
-CREATE TABLE IF NOT EXISTS UserShop (
-    user_id INTEGER,
-    shop_id INTEGER,
-	personal_photo TEXT NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES Users(user_id),
-    FOREIGN KEY (shop_id) REFERENCES Shops(shop_id)
-);
